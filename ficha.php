38d3f30f0ed70071ac3b4718911dfd1ebee06f68
Anthenor Fernandes <149077468+AnthenorFer@users.noreply.github.com>
2024-10-04 09:55:08 -0300

    Update ficha.php

--- ficha.php
+++ ficha.php
@@ -1,43 +1,70 @@
 <?php
+
 $host = "127.0.0.1";
 $usuario = "root";
-$senha= "";
-$db="tavelaavaliacao";
-$conn = new mysqli($host,$usuario, $senha, $db);
+$senha = "";
+$db = "dbinferno";
 
-if ($conn->connect_error) 
-{
+$conn = new mysqli($host, $usuario, $senha, $db);
+
+
+if ($conn->connect_error) {
     die("Falha na conexão: " . $conn->connect_error);
 }
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST')
-{
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $nome = $conn->real_escape_string($_POST['nome']);
-    $email = $conn->real_escape_string($_POST['email']);
-    $senha = password_hash($_POST['senha'], PASSWORD_BCRYPT);
+    $objeto = $conn->real_escape_string($_POST['objeto']);
+    $historia = $conn->real_escape_string($_POST['historia']);
+    $classe = $conn->real_escape_string($_POST['classe']);
+    $vida = intval($_POST['vida']);
+    $vitalidade = intval($_POST['vitalidade']);
+    $maldicao = intval($_POST['maldicao']);
+    $astucia = intval($_POST['astucia']);
+    $presteza = intval($_POST['presteza']);
+    $auge = intval($_POST['auge']);
+    $crueldade = intval($_POST['crueldade']);
+    $obstinacao = intval($_POST['obstinacao']);
+    $violacao = intval($_POST['violacao']);
+    $id = intval($_POST['id']);
+    $id_usuario_ficha = intval($_POST['id_usuario_ficha']);
 
-    $sql = "INSERT INTO usuarios (nome, email, senha) VALUES ('$nome', '$email', '$senha')";
 
-    if ($conn->query($sql) === TRUE) 
-    {
-        header('Location: index.php');
+    //$vantagens = somatória dos pontos de vantagens atribuidos baseado na classe escolhida (não sei como vou fazer ainda)
+    //$desvantagens = a mesma coisa em cima
+
+
+
+    /*if ($vantagens == 10 && $desvantagens == 10){
+        $sql = "INSERT INTO fichapersonagem (nome, objeto, historia, classe, vida, vitalidade, maldicao, astucia, presteza, auge, crueldade, obstinacao, violacao, id) VALUES ('$nome', '$historia', '$classe', '$objeto', '$vida', '$vitalidade', '$maldicao', '$astucia', '$presteza', '$auge', '$crueldade', '$obstinacao', '$violacao', '$id', '$id_usuario_ficha')";
+    }else{
+        echo "Não adianta tentar passar a perna, corrija seus pontos de Vantagens e Desvantagens!!";
+    }*/
+
+
+
+    if ($conn->query($sql) === TRUE) {
+        header('Location: ficha.php');
         exit;
     } else {
         echo "Erro: " . $sql . "<br>" . $conn->error;
-        }
-} 
+    }
+}
 $conn->close();
 
-//$nome = $_POST['nome'];
-//$sql = "INSERT INTO usuarios (nome) VALUES ('$nome')";
-// Se o usuário inserir "Joana D'arc", a consulta se torna:
-// INSERT INTO usuarios (nome) VALUES ('Joana 'D'Arac')
-// Isso pode causar um erro SQL ou ser explorado em um ataque.
-
-//$nome = $conn->real_escape_string($_POST['nome']);
-//$sql = "INSERT INTO usuarios (nome) VALUES ('$nome')";
-// A string é segura, pois "Joana D'arc" será transformado em "Joana \D'Arc'".
-// A consulta se torna segura:
-// INSERT INTO usuarios (nome) VALUES ("Joana \D'Arc")
-
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
